3fffe47d8ec45bbfc8e579a3304f880735c8f565
Benjamin <bvanstaden@singular.co.za>
2022-07-26 14:46:57 +0200

    End of section 7

--- cleint-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ cleint-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,37 +1,26 @@
-import React from "react";
+import { observer } from "mobx-react-lite";
 import { Grid } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
+import { useStore } from "../../../app/stores/store";
 import ActivityDetails from "../details/ActivityDetails";
-import ActvityList from "./ActivityList";
 import ActivityList from "./ActivityList";
 import ActivityFrom from "./form/ActivityForm";
 
-interface Props{
-    activities: Activity[];
-    selectedActivity: Activity | undefined;
-    selectActivity: (id:string) =>void;
-    cancelSelectActivity: () => void;
-    editMode: boolean;
-    openForm: (id: string) => void;
-    closeForm: () => void;
-    createOrEdit: (activity: Activity) => void;
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-  }
+export default observer(function ActivityDashboard(){
 
-export default function ActivityDashboard({activities, selectedActivity, selectActivity, cancelSelectActivity,
-     editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting}: Props){
+    const {activityStore} = useStore();
+    const {selectedActivity, editMode} = activityStore;
     return(
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} submitting={submitting}/>
+                <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
-                <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSelectActivity} openForm={openForm}/>}
+                <ActivityDetails/>}
                 {editMode &&
-                <ActivityFrom closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} submitting={submitting}/>}
+                <ActivityFrom />}
             </Grid.Column>
         </Grid>
     )
 }
+)
