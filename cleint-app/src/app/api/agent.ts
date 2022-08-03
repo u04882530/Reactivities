11b6f5cc8a9b67535de5cc5b812719791d8792ea
Benjamin <bvanstaden@singular.co.za>
2022-08-03 11:52:59 +0200

    End of section 17

--- cleint-app/src/app/api/agent.ts
+++ cleint-app/src/app/api/agent.ts
@@ -4,6 +4,7 @@ import { Activity, ActivityFormValues } from "../models/activity";
 import { history } from "../..";
 import { store } from "../stores/store";
 import { User, UserFormValues } from "../models/user";
+import { Photo, Profile } from "../models/profile";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) =>{
@@ -79,9 +80,23 @@ const Account = {
     register: (user: UserFormValues) => request.post<User>('/account/register', user)
 }
 
+const Profiles = {
+    get: (username: string) => request.get<Profile>(`/profiles/${username}`),
+    uploadPhoto: (file: Blob) => {
+        let formData = new FormData();
+        formData.append('File', file);
+        return axios.post<Photo>('photos', formData, {
+            headers: {'Content-type': 'multipart/form-data'}
+        })
+    },
+    setMainPhoto: (id:string) => request.post(`/photos/${id}/setMain`, {}),
+    deletePhoto: (id: string) => request.del(`/photos/${id}`)
+}
+
 const agent = {
     Activities,
-    Account
+    Account,
+    Profiles
 }
 
 export default agent;
